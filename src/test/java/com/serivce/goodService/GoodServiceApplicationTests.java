513e937fea34019d9ba154fda212c773653a3449
TayLee <thlee991@gmail.com>
2023-09-07 16:03:24 +0900

    commit) find static

--- src/test/java/com/serivce/goodService/GoodServiceApplicationTests.java
+++ src/test/java/com/serivce/goodService/GoodServiceApplicationTests.java
@@ -1,7 +1,9 @@
 package com.serivce.goodService;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
@@ -14,18 +16,18 @@ import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 // import org.springframework.boot.test.context.SpringBootTest;
 
+import com.serivce.goodService.first_service.service.GoodService;
 import com.serivce.goodService.first_service.utility.GoodUtility;
 
 // @SpringBootTest
 class GoodServiceApplicationTests {
 
-    static MockedStatic<GoodUtility> staticMocked;
+    // static MockedStatic<GoodUtility> staticMocked;
 
-    @BeforeAll
-    public static void beforeAll(){
-        staticMocked = mockStatic(GoodUtility.class);
-    }
-
+    // @BeforeAll
+    // public static void beforeAll(){
+    //     staticMocked = mockStatic(GoodUtility.class);
+    // }
 
 	@Test
 	void contextLoads() {
@@ -60,7 +62,8 @@ class GoodServiceApplicationTests {
 
     @Test
     public void verfiyTest(){
-        System.out.println(anyInt());
+        // System.out.println(anyInt()); /anyInt()는 아무때나 쓸수는 없다
+        System.out.println("good_verfiy_Test");
     }
 
     @Test
@@ -68,10 +71,18 @@ class GoodServiceApplicationTests {
         /*static method is belong to class, not in instance. so Mockito library need to make something like proxy class with dynamic proxy technique
         that is mockStatic()*/
 
-        String[] result = new String[] {"a", "b", "c"};
-        when(GoodUtility.parsingMethod("a b c")).thenReturn(result);
-
-        assertEquals(result, GoodUtility.parsingMethod("a b c"));
+         // 정적 메서드를 모의(mock)하는 방식
+         try (MockedStatic<GoodUtility> staticMocked = mockStatic(GoodUtility.class)) {
+            // 모의(mock) 객체를 사용하여 메서드 호출 결과를 설정합니다.
+            String[] expectedResult = new String[] {"a", "b", "c"};
+            staticMocked.when(() -> GoodUtility.parsingMethod(Mockito.anyString())).thenReturn(expectedResult);
+        
+            // 테스트 대상 코드에서 정적 메서드를 호출합니다.
+            String[] actualResult = GoodUtility.parsingMethod("a b c");
+        
+            // 결과를 검증합니다.
+            assertArrayEquals(expectedResult, actualResult);
+        }
 
         /**
          * The used MockMaker SubclassByteBuddyMockMaker does not support the creation of static mocks
