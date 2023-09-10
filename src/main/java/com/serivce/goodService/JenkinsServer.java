a3f4bedf54dc2a9ac1a0b1f4c14a244cc4ccd7ed
TAYLEE <89365465+TAEHYUKLEE@users.noreply.github.com>
2023-09-11 00:40:39 +0900

    commit) jenkinsServer

---
+++ src/main/java/com/serivce/goodService/JenkinsServer.java
@@ -0,0 +1,13 @@
+package com.serivce.goodService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsServer.class, args);
+	}
+
+}
