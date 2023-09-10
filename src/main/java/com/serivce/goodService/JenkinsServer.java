2498f1c1c5bf48000d829205ba365d055a8d6c0e
TAYLEE <89365465+TAEHYUKLEE@users.noreply.github.com>
2023-09-11 01:00:26 +0900

    check of pipeline

--- src/main/java/com/serivce/goodService/JenkinsServer.java
+++ src/main/java/com/serivce/goodService/JenkinsServer.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsServer {
 
 	public static void main(String[] args) {
+		//JenkinsServer에 배포됨.
 		SpringApplication.run(JenkinsServer.class, args);
 	}
 
